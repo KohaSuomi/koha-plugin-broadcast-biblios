3e1497c8a38bde6d72eb0b1707805371f2d4b77e
Johanna Raisa <johanna.raisa@gmail.com>
2021-10-07 11:58:04 +0300

    Add branch check to deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,12 @@ PM_FILE="Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios.pm"
 VERSION=`grep -oE "\-?[0-9]+\.[0-9]+\.[0-9]" $PM_FILE | head -1`
 MINVERSION=`grep -oE "\-?[0-9]+\.[0-9][0-9]" $PM_FILE | head -1`
 RELEASE_FILE="koha-plugin-broadcast-biblios-v${VERSION}.kpz"
-VERSIONTAG=`git tag -l "v${VERSION}-koha-$MINVERSION"`
+CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+if [ "$CURRENT_BRANCH" == "master" ]; then
+    VERSIONTAG=`git tag -l "v${VERSION}-koha-$MINVERSION"`
+else
+    VERSIONTAG=`git tag -l "v${VERSION}-koha-$MINVERSION-$CURRENT_BRANCH"`
+fi
 
 if [ $VERSIONTAG ]; then
     echo "Release version already exists!"
@@ -18,5 +23,8 @@ echo "Building release package ${RELEASE_FILE}"
 zip -r $RELEASE_FILE ./Koha
 
 echo "Creating tag v${VERSION}-koha-$MINVERSION"
-
-git tag -a "v${VERSION}-koha-$MINVERSION" -m "Release ${VERSION}"
+if [ "$CURRENT_BRANCH" == "master" ]; then
+    git tag -a "v${VERSION}-koha-$MINVERSION" -m "Release ${VERSION}"
+else
+    git tag -a "v${VERSION}-koha-$MINVERSION-$CURRENT_BRANCH" -m "Release ${VERSION}-$CURRENT_BRANCH"
+fi
