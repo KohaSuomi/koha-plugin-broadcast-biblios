ea4ad090e9d6eb50a806183e5fbafc310ae39869
Johanna Raisa <johanna.raisa@gmail.com>
2021-10-07 12:04:03 +0300

    Add branch to kpz file

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,13 @@
 PM_FILE="Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios.pm"
 VERSION=`grep -oE "\-?[0-9]+\.[0-9]+\.[0-9]" $PM_FILE | head -1`
 MINVERSION=`grep -oE "\-?[0-9]+\.[0-9][0-9]" $PM_FILE | head -1`
-RELEASE_FILE="koha-plugin-broadcast-biblios-v${VERSION}.kpz"
 CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 if [ "$CURRENT_BRANCH" == "master" ]; then
     VERSIONTAG=`git tag -l "v${VERSION}-koha-$MINVERSION"`
+    RELEASE_FILE="koha-plugin-broadcast-biblios-v${VERSION}.kpz"
 else
     VERSIONTAG=`git tag -l "v${VERSION}-koha-$MINVERSION-$CURRENT_BRANCH"`
+    RELEASE_FILE="koha-plugin-broadcast-biblios-v${VERSION}-$CURRENT_BRANCH.kpz"
 fi
 
 if [ $VERSIONTAG ]; then
@@ -22,9 +23,10 @@ echo "Building release package ${RELEASE_FILE}"
 
 zip -r $RELEASE_FILE ./Koha
 
-echo "Creating tag v${VERSION}-koha-$MINVERSION"
 if [ "$CURRENT_BRANCH" == "master" ]; then
+    echo "Creating tag v${VERSION}-koha-$MINVERSION"
     git tag -a "v${VERSION}-koha-$MINVERSION" -m "Release ${VERSION}"
 else
+    echo "Creating tag v${VERSION}-koha-$MINVERSION-$CURRENT_BRANCH"
     git tag -a "v${VERSION}-koha-$MINVERSION-$CURRENT_BRANCH" -m "Release ${VERSION}-$CURRENT_BRANCH"
 fi
