bb62f52f72fc0e6b2f1d67ed2e1e2c3db3aedb35
Johanna Raisa <johanna.raisa@gmail.com>
2021-10-19 13:43:53 +0300

    Add deployment fixes

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,14 @@
 PM_FILE="Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios.pm"
 VERSION=`grep -oE "\-?[0-9]+\.[0-9]+\.[0-9]" $PM_FILE | head -1`
 MINVERSION=`grep -oE "\-?[0-9]+\.[0-9][0-9]" $PM_FILE | head -1`
-RELEASE_FILE="koha-plugin-broadcast-biblios-v${VERSION}.kpz"
-VERSIONTAG=`git tag -l "v${VERSION}-koha-$MINVERSION"`
+CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+if [ "$CURRENT_BRANCH" == "master" ]; then
+    VERSIONTAG=`git tag -l "v${VERSION}-koha-$MINVERSION"`
+    RELEASE_FILE="koha-plugin-broadcast-biblios-v${VERSION}.kpz"
+else
+    VERSIONTAG=`git tag -l "v${VERSION}-koha-$MINVERSION-$CURRENT_BRANCH"`
+    RELEASE_FILE="koha-plugin-broadcast-biblios-v${VERSION}-$CURRENT_BRANCH.kpz"
+fi
 
 if [ $VERSIONTAG ]; then
     echo "Release version already exists!"
@@ -17,6 +23,10 @@ echo "Building release package ${RELEASE_FILE}"
 
 zip -r $RELEASE_FILE ./Koha
 
-echo "Creating tag v${VERSION}-koha-$MINVERSION"
-
-git tag -a "v${VERSION}-koha-$MINVERSION" -m "Release ${VERSION}"
+if [ "$CURRENT_BRANCH" == "master" ]; then
+    echo "Creating tag v${VERSION}-koha-$MINVERSION"
+    git tag -a "v${VERSION}-koha-$MINVERSION" -m "Release ${VERSION}"
+else
+    echo "Creating tag v${VERSION}-koha-$MINVERSION-$CURRENT_BRANCH"
+    git tag -a "v${VERSION}-koha-$MINVERSION-$CURRENT_BRANCH" -m "Release ${VERSION}-$CURRENT_BRANCH"
+fi
