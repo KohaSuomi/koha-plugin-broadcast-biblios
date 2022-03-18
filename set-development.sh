86cbbbac0629600c023a86e7ae8df6e68fa9571c
Johanna Raisa <johanna.raisa@gmail.com>
2022-03-18 08:19:58 +0200

    Add script dir to set-development

--- set-development.sh
+++ set-development.sh
@@ -6,8 +6,10 @@ kohadir="$(grep -Po '(?<=<intranetdir>).*?(?=</intranetdir>)' $KOHA_CONF)"
 rm -r $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios
 rm $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios.pm
 
-ln -s "/home/jraisa//koha-plugin-broadcast-biblios/Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios
-ln -s "/home/jraisa//koha-plugin-broadcast-biblios/Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios.pm" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios.pm
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+ln -s "$SCRIPT_DIR/koha-plugin-broadcast-biblios/Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios
+ln -s "$SCRIPT_DIR/koha-plugin-broadcast-biblios/Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios.pm" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios.pm
 
 DATABASE=`xmlstarlet sel -t -v 'yazgfs/config/database' $KOHA_CONF`
 HOSTNAME=`xmlstarlet sel -t -v 'yazgfs/config/hostname' $KOHA_CONF`
