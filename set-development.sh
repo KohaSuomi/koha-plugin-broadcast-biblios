0c7fadd3d85546fc51b8bc31aede2b9ea52c2202
Johanna Raisa <johanna.raisa@gmail.com>
2022-03-21 09:53:15 +0200

    Fix script path

--- set-development.sh
+++ set-development.sh
@@ -8,8 +8,8 @@ rm $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios.pm
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-ln -s "$SCRIPT_DIR/koha-plugin-broadcast-biblios/Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios
-ln -s "$SCRIPT_DIR/koha-plugin-broadcast-biblios/Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios.pm" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios.pm
+ln -s "$SCRIPT_DIR/Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios
+ln -s "$SCRIPT_DIR/Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios.pm" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios.pm
 
 DATABASE=`xmlstarlet sel -t -v 'yazgfs/config/database' $KOHA_CONF`
 HOSTNAME=`xmlstarlet sel -t -v 'yazgfs/config/hostname' $KOHA_CONF`
