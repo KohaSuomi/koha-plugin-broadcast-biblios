1a4a59a9ff34df07f5ba4f118484da5bb0c2a25e
Johanna Raisa <johanna.raisa@gmail.com>
2022-08-29 08:26:21 +0300

    Skip component parts from active records

--- Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios/Modules/ActiveRecords.pm
+++ Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios/Modules/ActiveRecords.pm
@@ -58,6 +58,7 @@ sub getActiveRecordsByBiblionumber {
         my $biblios = $newbiblios->fetch();
         my $count = 0;
         foreach my $biblio (@{$biblios}) {
+            next if $self->checkComponentPart(MARC::Record::new_from_xml($biblio->{metadata}, 'UTF-8'));
             my ($identifier, $identifier_field) = $self->getActiveField($biblio);
             my $target_id = $biblio->{biblionumber};
             my $updated = $biblio->{timestamp};
