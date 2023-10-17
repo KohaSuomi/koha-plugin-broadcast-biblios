67864fa954cf17ae13d2b409ecb527db3a58538d
Johanna Raisa <johanna.raisa@gmail.com>
2023-10-17 15:20:23 +0300

    Add authentication functions

--- Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios/Modules/Users.pm
+++ Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios/Modules/Users.pm
@@ -26,6 +26,8 @@ use Koha::Plugin::Fi::KohaSuomi::BroadcastBiblios;
 use Koha::Plugin::Fi::KohaSuomi::BroadcastBiblios::Modules::Database;
 use C4::Context;
 use Crypt::JWT;
+use Mojo::UserAgent;
+use Koha::Logger;
 
 =head new
 
@@ -47,24 +49,85 @@ sub db {
     return Koha::Plugin::Fi::KohaSuomi::BroadcastBiblios::Modules::Database->new;
 }
 
+sub getConfig {
+    my ($self) = @_;
+    return shift->{_params}->{config};
+}
+
+sub getEndpoint {
+    my ($self) = @_;
+    return shift->{_params}->{endpoint};
+}
+
+sub ua {
+    my ($self) = @_;
+    return Mojo::UserAgent->new;
+}
+
+sub getLogger {
+    return Koha::Logger->get({instance => 'broadcast'});
+}
+
 sub addUser {
 
 }
 
 sub getAuthentication {
     my ($self, $user_id) = @_;
-    my $user = $self->db->getUserByUserId($user_id);
-    if ($user->{auth_type} eq "basic") {
 
+    my $user = $self->db->getUserByUserId($user_id);
+
+    if ($user->{auth_type} eq "basic") {
+        return $self->basicAuth($user);
     }
+
+    if ($user->{auth_type} eq "oauth") {
+        return $self->OAUTH2($user);
+    }
+
+    return 0;
 }
 
 sub basicAuth {
-
+    my ($self, $user) = @_;
+    my $endpoint = $self->getConfig->{baseUrl}.'/'.$self->getConfig->{$self->getEndpoint}->{path};
+    my $authentication = $user->{username} . ":" . $user->{password};
+    my $path = Mojo::URL->new($endpoint)->userinfo($authentication);
+    my $headers = {'Content-Type' => 'application/json'};
+    return ($path, $headers);
 }
 
 sub OAUTH2 {
+    my ($self, $user) = @_;
+    my $path = $self->getConfig->{baseUrl}.'/'.$self->getConfig->{$self->getEndpoint}->{path};
+    my $headers = {'Content-Type' => 'application/json', 'Authorization' => 'Bearer ' . $self->getAccessToken($user)};
+    return ($path, $headers);
+}
+
+sub getAccessToken {
+    my ($self, $user) = @_;
     
+    if ($user->{token_expires} < time()) {
+        return $self->refreshAccessToken($user);
+    } else {
+        return $user->{access_token};
+    }
+}
+
+sub refreshAccessToken {
+    my ($self, $user) = @_;
+    my $tx = $self->ua->post($user->{access_token_url} => form => {client_id => $user->{client_id}, client_secret => $user->{client_secret}, grant_type => $user->{grant_type}});
+    
+    if ($tx->res->error) {
+        $self->getLogger->error($tx->res->error->{message} . " " . $tx->res->body);
+        $self->db->updateAccessToken($user->{id}, undef, undef);
+        return 0;
+    }
+
+    my $res = $tx->res->json;
+    $self->db->updateAccessToken($user->{id}, $res->{access_token}, time() + $res->{expires_in});
+
+    return $res->{access_token};
 }
 
 1;
