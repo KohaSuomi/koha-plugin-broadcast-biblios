94a2ff86d72f6448f065d5a492ab95b0b13fb9f4
Johanna Raisa <johanna.raisa@gmail.com>
2023-10-24 12:13:21 +0300

    Add get and update functions to controller

--- Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios/Controllers/ActiveRecordController.pm
+++ Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios/Controllers/ActiveRecordController.pm
@@ -33,16 +33,65 @@ sub get {
 
     try {
 
+        my $activeRecords = Koha::Plugin::Fi::KohaSuomi::BroadcastBiblios::Modules::ActiveRecords->new();
+        my $biblio_id = $c->validation->param('biblio_id');
+        my $activeRecord = $activeRecords->getActiveRecordByBiblionumber($biblio_id);
+
+        unless ($activeRecord) {
+            return $c->render(status => 404, openapi => {error => "Activation not found for ".$biblio_id});
+        }
+
+        return $c->render(status => 200, openapi => $activeRecord);
+    } catch {
+        my $error = $_;
+        $logger->error($error);
+        return $c->render(status => 500, openapi => {error => "Something went wrong, check the logs"});
+    }
+}
+
+sub find {
+    my $c = shift->openapi->valid_input or return;
+
+    my $logger = Koha::Logger->get({ interface => 'api' });
+
+    try {
+
         my $activeRecords = Koha::Plugin::Fi::KohaSuomi::BroadcastBiblios::Modules::ActiveRecords->new({table => 'activerecords'});
         my $identifier = $c->validation->param('identifier');
         my $identifier_field = $c->validation->param('identifier_field');
         my $activeRecord = $activeRecords->getActiveRecordByIdentifier($identifier, $identifier_field);
 
         unless ($activeRecord) {
-            return $c->render(status => 404, openapi => {error => "Activation not found"});
+            return $c->render(status => 404, openapi => {error => "Activation not found for ".$identifier." ".$identifier_field});
         }
 
         return $c->render(status => 200, openapi => $activeRecord);
+    } catch {
+        my $error = $_;
+        $logger->error($error);
+        return $c->render(status => 500, openapi => {error => "Something went wrong, check the logs"});
+    }
+}
+
+sub update {
+    my $c = shift->openapi->valid_input or return;
+
+    my $logger = Koha::Logger->get({ interface => 'api' });
+
+    try {
+
+        my $biblio_id = $c->validation->param('biblio_id');
+        my $body = $c->req->json;
+        my $activeRecords = Koha::Plugin::Fi::KohaSuomi::BroadcastBiblios::Modules::ActiveRecords->new();
+        my $activeRecord = $activeRecords->getActiveRecordByBiblionumber($biblio_id);
+
+        unless ($activeRecord) {
+            return $c->render(status => 404, openapi => {error => "Activation not found for ".$biblio_id});
+        }
+
+        my $update = $activeRecords->updateActiveRecord($activeRecord->{id}, $body);
+
+        return $c->render(status => 200, openapi => {message => "Success"});
     } catch {
         my $error = $_;
         $logger->error($error);
