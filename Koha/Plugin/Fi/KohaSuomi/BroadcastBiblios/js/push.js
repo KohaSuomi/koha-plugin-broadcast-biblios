8f0e4ce757ef341f736617b67e6657ec327a5eea
Johanna Raisa <johanna.raisa@gmail.com>
2024-02-02 13:26:02 +0200

    Add overflow hidden to list elements

--- Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios/js/push.js
+++ Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios/js/push.js
@@ -289,13 +289,13 @@ const recordModal = Vue.component('recordmodal', {
       if (record) {
         var html = '<div>';
         html +=
-          '<li class="row" style="list-style:none;"> <div class="col-xs-3 mr-2">';
+          '<li class="row" style="list-style:none; overflow:hidden;"> <div class="col-xs-3 mr-2">';
         html +=
           '<b>000</b></div><div class="col-xs-9">' + record.leader + '</li>';
         record.fields.forEach(function (v, i, a) {
           if ($.isNumeric(v.tag)) {
             html +=
-              '<li class="row" style="list-style:none;"><div class="col-xs-3 mr-2">';
+              '<li class="row" style="list-style:none; overflow:hidden;"><div class="col-xs-3 mr-2">';
           } else {
             html += '<li class="row hidden"><div class="col-xs-3  mr-2">';
           }
