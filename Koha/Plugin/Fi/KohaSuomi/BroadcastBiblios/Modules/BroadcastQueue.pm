c1700314246b7d983e6a38f467abbc7980f6a1b9
Johanna Raisa <johanna.raisa@gmail.com>
2024-04-16 08:18:42 +0300

    Check componentpart count

--- Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios/Modules/BroadcastQueue.pm
+++ Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios/Modules/BroadcastQueue.pm
@@ -385,7 +385,7 @@ sub processExportComponentParts {
 
     $componentparts = $self->getComponentParts->sortComponentParts($componentparts);
     $broadcastcomponentparts = $self->getComponentParts->sortComponentParts($broadcastcomponentparts);
-    if ($method eq 'PUT' && $broadcastcomponentparts && scalar @$componentparts != scalar @$broadcastcomponentparts) {
+    if ($method eq 'PUT' && scalar @$broadcastcomponentparts > 0 && scalar @$componentparts != scalar @$broadcastcomponentparts) {
         die "Component parts count mismatch, local: ".scalar @$componentparts.", broadcast: ".scalar @$broadcastcomponentparts."\n";
     }
 
@@ -397,7 +397,7 @@ sub processExportComponentParts {
         my $comprecord = $self->getRecord($componentpart->{marcxml});
         my $broadcast_biblio_id;
 
-        if ($method eq 'PUT' && $broadcastcomponentparts) {
+        if ($method eq 'PUT' && scalar @$broadcastcomponentparts > 0) {
             $broadcast_biblio_id = $broadcastcomponentparts->[$i]->{biblionumber};
         }
 
