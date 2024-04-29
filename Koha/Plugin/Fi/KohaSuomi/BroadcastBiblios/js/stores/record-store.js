22c52481d16535fc464fbb13305ccb4f4bdbdeaa
Johanna Raisa <johanna.raisa@gmail.com>
2024-04-29 08:13:47 +0300

    Check if error.response is present

--- Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios/js/stores/record-store.js
+++ Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios/js/stores/record-store.js
@@ -34,7 +34,7 @@ export const useRecordStore = defineStore("record", {
         return response;
       } catch (error) {
         const errorStore = useErrorStore();
-        if (error.response.status != 404) {
+        if (error.response && error.response.status != 404) {
           errorStore.setError(error);
         }
       }
