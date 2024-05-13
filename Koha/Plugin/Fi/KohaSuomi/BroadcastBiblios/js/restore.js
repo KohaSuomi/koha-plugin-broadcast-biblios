15cb0c2a38b0b5e7eacb8d47ed46d1316f60457e
Johanna Raisa <johanna.raisa@gmail.com>
2024-05-13 10:05:44 +0300

    Test if loginfoRow has BEFORE=>

--- Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios/js/restore.js
+++ Koha/Plugin/Fi/KohaSuomi/BroadcastBiblios/js/restore.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
             rows.forEach(function(row) {
                 if (row.cells[2].textContent.includes("Catalog") && row.cells[3].textContent.includes("Modify")) {
                     var loginfoRow = row.querySelector(".loginfo");
-                    if (loginfoRow) {
+                    if (loginfoRow && /BEFORE=>/.test(loginfoRow.textContent)) {
                         let actionId = loginfoRow.id.match(/\d+/g)[0];
                         var restoreButton = document.createElement("button");
                         restoreButton.textContent = "Palauta";
